e8826c084fd89cf26d8db9b7b388be8e37ed28fa
Kareem Essam <65365457+kareemessam09@users.noreply.github.com>
2026-09-09 06:09:00 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val n = nums.size
+        val result = IntArray(n)
+
+        result[0] = 1
+        for (i in 1 until n) {
+            result[i] = result[i - 1] * nums[i - 1]
+        }
+
+        var suffix = 1
+        for (i in n - 1 downTo 0) {
+            result[i] *= suffix
+            suffix *= nums[i]
+        }
+
+        return result
+    }
+}
